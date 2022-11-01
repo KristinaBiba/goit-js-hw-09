6fe4001ba43d6ad137422b518ee726befab1244e
KristinaBiba <106387313+KristinaBiba@users.noreply.github.com>
2022-11-01 11:14:11 +0000

    Deploying to gh-pages from @ KristinaBiba/goit-js-hw-09@a2bc9b9e4f2f35abe8fb3d8d7cce5182c5b13773 🚀

--- 01-color-switcher.59339bfb.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");let n=!1;const r=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3);t.addEventListener("click",(function(){n||(n=!0)})),e.addEventListener("click",(function(){n=!1,clearInterval(r)}));
-//# sourceMappingURL=01-color-switcher.59339bfb.js.map
